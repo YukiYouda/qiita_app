1425f626434cffc956d0473dab3a8ac9321c0703
YukiYouda <youda0518@yahoo.co.jp>
2021-09-25 17:59:10 +0900

    CRUD処理完成

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\View\ViewServiceProvider;
+use GuzzleHttp\Client;
+use PhpParser\Node\Stmt\Catch_;
 
 class ArticleController extends Controller
 {
@@ -13,7 +16,51 @@ class ArticleController extends Controller
      */
     public function index()
     {
-        //
+        $method = 'GET';
+        $tags = 'PHP';
+        $per_page = 30;
+
+
+        $url = config('qiita.url') . '/api/v2/tags/' . $tags . '/items?' . 'page=1&' . 'per_page=' . $per_page;
+
+        $options = [
+            'headers' => [
+                'Authorization' => 'Bearer ' . config('qiita.token')
+            ],
+        ];
+
+        $client = new Client();
+
+        try {
+            $response = $client->request($method, $url, $options);
+            $body = $response->getBody();
+            $articles = json_decode($body, false);
+        } catch (\Throwable $th) {
+            $articles = null;
+        }
+
+        /// ここから自分の投稿記事取得
+        $method = 'GET';
+
+        $url = config('qiita.url') . '/api/v2/authenticated_user/items'; 
+
+        $options = [
+            'headers' => [
+                'Authorization' => 'Bearer ' . config('qiita.token')
+            ],
+        ];
+
+        $client = new Client();
+
+        try {
+            $response = $client->request($method, $url, $options);
+            $body = $response->getBody();
+            $myarticles = json_decode($body, false);
+        } catch (\Throwable $th) {
+            $myarticles = null;
+        }
+
+        return view('articles.index')->with(compact('articles', 'myarticles')); 
     }
 
     /**
@@ -23,7 +70,7 @@ class ArticleController extends Controller
      */
     public function create()
     {
-        //
+        return view('articles.create');
     }
 
     /**
@@ -34,7 +81,44 @@ class ArticleController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $method = 'POST';
+
+        $url = config('qiita.url') . '/api/v2/items';
+
+        
+        $tag_array = explode(' ', $request->tags);
+        $tags= array_map(function($tag){
+            return ['name' => $tag];
+        }, $tag_array);
+
+        $data = [
+            'title' => $request->title,
+            'body' => $request->body,
+            'private' => $request->private == 'true' ? true : false,
+            'tags' => $tags
+        ];
+
+        $options = [
+            'headers' => [
+                'Authorization' => 'Bearer ' . config('qiita.token'),
+                'Content-Type' => 'application/json'
+            ],
+            'json' => $data,
+        ];
+
+        $client = new Client();
+
+        try {
+            $response = $client->request($method, $url, $options);
+            $body = $response->getBody();
+            $article = json_decode($body, false);
+        } catch (\GuzzleHttp\Exception\ClientException $e) {
+            // return back()->withErrors(['error' => $e->getResponse()->getReasonPhrase()]);
+            return back()->withErrors(['error' => '投稿に失敗しました!']);
+        }
+
+        $message = new \Illuminate\Support\HtmlString("記事の投稿に成功しました");
+        return redirect()->route('articles.index')->with('flash_message', $message); 
     }
 
     /**
@@ -45,7 +129,56 @@ class ArticleController extends Controller
      */
     public function show($id)
     {
-        //
+        $method = 'GET';
+
+        $url = config('qiita.url') . '/api/v2/items/' . $id;
+
+        $options = [
+            'headers' => [
+                'Authorization' => 'Bearer ' . config('qiita.token')
+            ],
+        ];
+
+        $client = new Client();
+
+        try {
+            $response = $client->request($method, $url, $options);
+            $body = $response->getBody();
+            $article = json_decode($body, false);
+
+            $parser = new \cebe\markdown\GithubMarkdown();
+            $parser->keepListStartNumber = true;
+            $parser->enableNewlines = true;
+
+            $html_string = $parser->parse($article->body);
+            $article->html = new \Illuminate\Support\HtmlString($html_string);
+
+        } catch (\Throwable $th) {
+            return back();
+        }
+
+        ///自分
+        $method = 'GET';
+
+        $url = config('qiita.url') . '/api/v2/authenticated_user/';
+
+        $options = [
+            'headers' => [
+                'Authorization' => 'Bearer ' . config('qiita.token')
+            ],
+        ];
+
+        $client = new Client();
+
+        try {
+            $response = $client->request($method, $url, $options);
+            $body = $response->getBody();
+            $user = json_decode($body, false);
+        } catch (\Throwable $th) {
+            return back();
+        }
+
+        return view('articles.show')->with(compact('article', 'user')); 
     }
 
     /**
@@ -56,7 +189,32 @@ class ArticleController extends Controller
      */
     public function edit($id)
     {
-        //
+        $method = 'GET';
+
+        $url = config('qiita.url') . '/api/v2/items/' . $id;
+
+        $options = [
+            'headers' => [
+                'Authorization' => 'Bearer ' . config('qiita.token')
+            ],
+        ];
+
+        $client = new Client();
+
+        try {
+            $response = $client->request($method, $url, $options);
+            $body = $response->getBody();
+            $article = json_decode($body, false);
+
+            $tag_array = array_map(function($tag){
+                return $tag->name;
+            }, $article->tags);
+            $article->tags = implode(' ', $tag_array);
+        } catch (\Throwable $th) {
+            return back();
+        }
+
+        return view('articles.edit')->with(compact('article')); 
     }
 
     /**
@@ -68,7 +226,44 @@ class ArticleController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $method = 'PATCH';
+
+        $url = config('qiita.url') . '/api/v2/items/' . $id;
+
+
+        $tag_array = explode(' ', $request->tags);
+        $tags = array_map(function ($tag) {
+            return ['name' => $tag];
+        }, $tag_array);
+
+        $data = [
+            'title' => $request->title,
+            'body' => $request->body,
+            'private' => $request->private == 'true' ? true : false,
+            'tags' => $tags
+        ];
+
+        $options = [
+            'headers' => [
+                'Authorization' => 'Bearer ' . config('qiita.token'),
+                'Content-Type' => 'application/json'
+            ],
+            'json' => $data,
+        ];
+
+        $client = new Client();
+
+        try {
+            $response = $client->request($method, $url, $options);
+            $body = $response->getBody();
+            $article = json_decode($body, false);
+        } catch (\GuzzleHttp\Exception\ClientException $e) {
+            // return back()->withErrors(['error' => $e->getResponse()->getReasonPhrase()]);
+            return back()->withErrors(['error' => '更新に失敗しました!']);
+        }
+
+        $message = new \Illuminate\Support\HtmlString("記事の更新に成功しました");
+        return redirect()->route('articles.index')->with('flash_message', $message); 
     }
 
     /**
@@ -79,6 +274,24 @@ class ArticleController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $method = 'DELETE';
+
+        $url = config('qiita.url') . '/api/v2/items/' . $id;
+
+        $options = [
+            'headers' => [
+                'Authorization' => 'Bearer ' . config('qiita.token')
+            ]
+        ];
+
+        $client = new Client();
+
+        try {
+            $client->request($method, $url, $options);
+        } catch (\GuzzleHttp\Exception\ClientException $e) {
+            return back()->withErrors(['error' => '削除処理を失敗しました']);
+        }
+
+        return redirect()->route('articles.index')->with('flash_message', '記事を削除しました');
     }
 }
